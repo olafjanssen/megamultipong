e87f93ce5242feafce812fff52568c6d9880bc31
Olaf Janssen <o.t.a.janssen@gmail.com>
2015-03-21 00:25:26 +0100

    Add paddle effect based on contact point.

--- scripts/multipong.js
+++ scripts/multipong.js
@@ -24,7 +24,9 @@ var multipong = (function (chain) {
 
         leftElement,
         rightElement,
-        middleElement;
+        middleElement,
+
+        sound = false;
 
     function toRelative(left, top) {
         return {left: left / window.innerWidth, top: top / window.innerHeight};
@@ -46,25 +48,28 @@ var multipong = (function (chain) {
     }
 
     function gameLoop() {
+        var newTime = new Date().getTime(),
+            delay = newTime - time;
+
         if (document.body.classList.contains('playing')) {
             requestAnimationFrame(gameLoop);
         }
 
-        var newTime = new Date().getTime(),
-            delay = newTime - time;
         time = newTime;
         balls.forEach(function (ball) {
-            var relPos,
-                newBall;
+            var ballElement = document.getElementById('ball' + ball.id),
+                relPos,
+                newBall,
+                angleOffset = 0;
 
             // update ball object
             ball.left += Math.cos(ball.angle) * ball.speed * delay;
             ball.top += Math.sin(ball.angle) * ball.speed * delay;
 
-            var ballElement = document.getElementById('ball' + ball.id);
             if (!ballElement) {
                 return;
             }
+
             // check edge collision
             var ballRect = ballElement.getBoundingClientRect();
             var dx = Math.cos(ball.angle),
@@ -113,7 +118,8 @@ var multipong = (function (chain) {
 
             if (isRight && ballRect.right > rightRect.left && dx > 0 && ballRect.top < rightRect.bottom && ballRect.bottom > rightRect.top) {
                 ball.left = rightRect.left - ballRect.width / 2;
-                dx = -Math.abs(dx + (Math.random() - 0.5) * 30 / 180 * Math.PI);
+                dx = -Math.abs(dx);
+                angleOffset = ((ballRect.top - rightRect.top + ballRect.height) / (rightRect.height + ballRect.height) - 0.5) * -30 / 180 * Math.PI;
                 ball.speed += 1.0 / 16.0;
                 hitPaddleSound.play();
                 sendMessage(Math.floor(devices / 2), {action: 'restart'});
@@ -121,7 +127,8 @@ var multipong = (function (chain) {
 
             if (isLeft && ballRect.left < leftRect.right && dx < 0 && ballRect.top < leftRect.bottom && ballRect.bottom > leftRect.top) {
                 ball.left = leftRect.right + ballRect.width / 2;
-                dx = Math.abs(dx + (Math.random() - 0.5) * 30 / 180 * Math.PI);
+                dx = Math.abs(dx);
+                angleOffset = ((ballRect.top - leftRect.top + ballRect.height) / (leftRect.height + ballRect.height) - 0.5) * 30 / 180 * Math.PI;
                 ball.speed += 1.0 / 16.0;
                 hitPaddleSound.play();
                 sendMessage(Math.floor(devices / 2), {action: 'restart'});
@@ -129,13 +136,14 @@ var multipong = (function (chain) {
 
             if (isMiddle && ballRect.left < middleRect.right && ballRect.right > middleRect.left && ballRect.top < middleRect.bottom && ballRect.bottom > middleRect.top) {
                 ball.left = dx > 0 ? middleRect.left - ballRect.width : middleRect.right + ballRect.width / 2;
-                dx = -dx + (Math.random() - 0.5) * 30 / 180 * Math.PI;
+                angleOffset = ((ballRect.top - middleRect.top + ballRect.height) / (middleRect.height + ballRect.height) - 0.5) * dx/Math.abs(dx)* -30 / 180 * Math.PI;
+                dx = -dx;
                 ball.speed += 1.0 / 16.0;
                 hitPaddleSound.play();
             }
 
             // update ball angle
-            ball.angle = Math.atan2(dy, dx);
+            ball.angle = Math.atan2(dy, dx) + angleOffset;
             // update positions in css
             ballElement.style.transform = 'translate(' + ball.left + 'px, ' + ball.top + 'px)';
         });
@@ -175,27 +183,42 @@ var multipong = (function (chain) {
 
         scoreSound = {
             audio: document.getElementById('score-sound'), play: function () {
-                this.audio.play()
+                if (sound) {
+                    this.audio.play()
+                }
+                ;
             }
         };
         hitPaddleSound = {
             audio: document.getElementById('hit-paddle-sound'), play: function () {
-                this.audio.play()
+                if (sound) {
+                    this.audio.play()
+                }
+                ;
             }
         };
         hitWallSound = {
             audio: document.getElementById('hit-wall-sound'), play: function () {
-                this.audio.play()
+                if (sound) {
+                    this.audio.play()
+                }
+                ;
             }
         };
         startSound = {
             audio: document.getElementById('start-sound'), play: function () {
-                this.audio.play()
+                if (sound) {
+                    this.audio.play()
+                }
+                ;
             }
         };
         gameOverSound = {
             audio: document.getElementById('game-over-sound'), play: function () {
-                this.audio.play()
+                if (sound) {
+                    this.audio.play()
+                }
+                ;
             }
         };
 
